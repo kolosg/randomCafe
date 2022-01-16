ffaf5c4afabd6d8730a3cce6618a7fb52015b536
kolosg <kolos460@gmail.com>
2022-01-16 12:25:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+    pass
+
+#reads the CSV file that contains the team names and their departments
+def read_csv():
+    pass
+#reads an private CSV that has all the combinations of people
+def read_combinations():
+    pass
+#creates a pair (always from a different department)
+def generate_pair():
+    pass
+#
+def check_pair():
+    pass
+
+def write_csv():
+    pass
+
+def
+
+if __name__ == '__main__':
+    main()
